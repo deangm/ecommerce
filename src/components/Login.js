02e3ad76088f5a37d217be160ab39e4f22594f9c
mikey <michael.d.deangelo@Gmail.com>
2020-01-02 08:01:56 -0700

    consolidates login and create account

--- src/components/Login.js
+++ src/components/Login.js
@@ -34,6 +34,33 @@ class Login extends React.Component {
         })
     }
 
+
+    handleCreateAccountClick = () => {
+
+        store.dispatch({
+            type: "CREATE_ACCOUNT",
+            newAccount: {
+                username: this.state.username,
+                password: this.state.password,
+            }
+        })
+
+        console.log(store.getState().loggedIn)
+    }
+
+    handleUsernameChange = (e) => {
+        this.setState({
+            username: e.target.value
+        })
+    }
+    handlePasswordChange = (e) => {
+        this.setState({
+
+            password: e.target.value
+
+        })
+    }
+
     render() {
 
         if (!store.getState().loggedIn.activeUser) {
@@ -45,24 +72,51 @@ class Login extends React.Component {
 
 
 
-                <div className = "cont">
-                    <div className = "subTitle">
+                <div className="cont">
+                    <div className="subTitle">
                         Welcome Back!
                     </div>
 
-                <div className = "loginCont">
-                    <div className = "inputCont">
-                        <label>UserName:</label>
-                        <input onChange={this.userNameHandler} value={this.state.username} placeholder={"username"}></input>
-                        <label>Password</label>
-                        <input type="password" onChange={this.passwordHandler} value={this.state.password} placeholder={"password"}></input>
+                    <div className="loginCont">
+                        <div className="inputCont">
+                            <label>UserName:</label>
+                            <input onChange={this.userNameHandler} value={this.state.username} placeholder={"username"}></input>
+                            <label>Password</label>
+                            <input type="password" onChange={this.passwordHandler} value={this.state.password} placeholder={"password"}></input>
+                        </div>
+                        <button className="btn btn-primary" onClick={this.handleLogin}>Log In</button>
+                        <div data-toggle="modal" data-target="#exampleModalCenter">
+                            Create Account
+                        </div>
                     </div>
-                    <button className = "btn btn-primary" onClick={this.handleLogin}>Log In</button>
-                    <Link to = "/CreateAccount">
-                        Create Account
-                    </Link>
+
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div>
+                                        <div>
+                                            <input value={this.state.username} onChange={this.handleUsernameChange} placeholder="username"></input>
+                                            <input value={this.state.password} onChange={this.handlePasswordChange} placeholder="password"></input>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <div onClick={this.handleCreateAccountClick} data-dismiss="modal">Create My Account</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
+
+
+
 
 
             )
